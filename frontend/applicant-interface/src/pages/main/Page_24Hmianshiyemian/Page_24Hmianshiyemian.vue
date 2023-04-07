3bcd9c681e479284d9daf47ed7a352806dd2d5e3
徐江 <1787297855@qq.com>
2023-04-07 22:29:06 +0800

    fix

--- frontend/applicant-interface/src/pages/main/Page_24Hmianshiyemian/Page_24Hmianshiyemian.vue
+++ frontend/applicant-interface/src/pages/main/Page_24Hmianshiyemian/Page_24Hmianshiyemian.vue
@@ -1,11 +1,42 @@
 <template>
   <view class="flex-col items-center justify-center page">
-    <image src="@/static/icons/noadvertise.svg" />
-    <text>暂无广告商投资</text>
+    <z-paging
+      ref="paging"
+      v-model="positions"
+      use-page-scroll
+      :default-page-size="5"
+      @query="queryList"
+    >
+      <job-detail
+        v-for="position in positions"
+        :key="position.id"
+        :position="position"
+        @job-click="jobDescription(position)"
+      />
+    </z-paging>
   </view>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type { Advertise } from '@dongjiang-recruitment/service-common';
+
+
+const positions = ref<Array<Advertise>>([]);
+const paging = ref<{ complete: (param: Array<any> | boolean) => void }>();
+const queryList = async (pageNo: number, pageSize: number) => {
+  paging.value?.complete(
+    (
+      await advertiserService.queryAllAdvertise({
+        query: {
+          position: ["$eq", 2]
+        },
+        page: pageNo,
+        size: pageSize,
+      })
+    ).items
+  );
+};
+</script>
 
 <style lang="scss" scoped>
 .page {
